06469fa8130859d689eb023c4e1261f78e446d2a
JacyHuang <j.c-h@qq.com>
2015-09-21 15:45:59 +0800

    123

--- js/new_file2.js
+++ js/new_file2.js
@@ -22,40 +22,67 @@
       };
       $scope.listCanSwipe = true;
       
-      $scope.showPopup = function(item) {
+      $scope.showPopup_Delete = function(item) {
         var index = $scope.items.indexOf(item);
         $scope.data = {}
 
           // An elaborate, custom popup
           var myPopup = $ionicPopup.show({
-            template: '<input type="text" ng-model="data.alias">',
             title: '',
-            subTitle: '修改名称',
+            subTitle: '是否删除？',
             scope: $scope,
             buttons: [
+            {
+              text: '<b>确定</b>',
+              type: 'button-positive',
+              onTap: function(e) {
+              	$scope.items.splice(index, 1);
+              }
+            },
             { 
-            	text: 'Cancel' ,
+            	text: '取消' ,
             	type: 'button-positive',
             	onTap: function(e) {
             		$ionicListDelegate.closeOptionButtons();  
             	}
-            },
-            {
-              text: '<b>Save</b>',
-              type: 'button-positive',
-              onTap: function(e) {
-              if (!$scope.data.alias) {
-              e.preventDefault();
-              } else {
-										alias     : $scope.data.alias
-                    $scope.items[index] = $scope.data.alias;
-                    $ionicListDelegate.closeOptionButtons();  
-                }
-              }
             }
             ]
-          });
-  
+          });  
 			}
+			
+		  $scope.showPopup_Edit = function(item) {
+      var index = $scope.items.indexOf(item);
+      $scope.data = {}
+
+        // An elaborate, custom popup
+        var myPopup = $ionicPopup.show({
+          template: '<input type="text" ng-model="data.alias">',
+          title: '',
+          subTitle: '修改名称',
+          scope: $scope,
+          buttons: [
+          {
+            text: '<b>保存</b>',
+            type: 'button-positive',
+            onTap: function(e) {
+            if (!$scope.data.alias) {
+            e.preventDefault();
+            } else {
+									alias     : $scope.data.alias
+                  $scope.items[index] = $scope.data.alias;
+                  $ionicListDelegate.closeOptionButtons();  
+              }
+            }
+          },
+          { 
+          	text: '取消' ,
+          	type: 'button-positive',
+          	onTap: function(e) {
+          		$ionicListDelegate.closeOptionButtons();  
+          	}
+          }
+          ]
+        });  
+		}
   
 });
