89a66586d516ca8d8c9fc66b27a91c927de34325
JacyHuang <j.c-h@qq.com>
2015-11-20 17:05:36 +0800

    00kkll

--- js/new_file2.js
+++ js/new_file2.js
@@ -4,9 +4,7 @@
   var light_id=2;
 
   	//light_list.push({'light_id':light_id, 'light_alias':light_alias, 'light_state':light_state});
-  	light_list.push({'light_id':1, 'light_alias':"红色", 'light_state':0});
-  	light_list.push({'light_id':2, 'light_alias':"绿色", 'light_state':0});
-  	light_list.push({'light_id':3, 'light_alias':"蓝色", 'light_state':0});
+
 
 angular.module('ionicApp', ['ionic'])
 
